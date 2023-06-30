58b262e4fc7b8ec3d4adcebc983d0da825acc614
alonr1807 <alonr1807@gmail.com>
2023-06-30 17:50:09 -0500

    Added search button for inputed item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useState } from 'react';
 import logo from './logo.png';
 import './App.css';
 
 function App() {
+  const [textBoxValue, setTextBoxValue] = useState('');
+
+  const handleTextBoxChange = (event) => {
+    setTextBoxValue(event.target.value);
+  };
+
+  // send to bard api to find materials
+  const handleButtonClick = () => {
+    console.log('Stored value:', textBoxValue);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +22,14 @@ function App() {
         
       </header>
       <div className="text-box">
-          <input type="text" placeholder="Enter your text" />
+          <input type="text" 
+          placeholder="Enter your item" 
+          value={textBoxValue}
+          onChange={handleTextBoxChange}
+          />
+          <button onClick={handleButtonClick}>
+            Store Value
+          </button>
         </div>
     </div>
   );
