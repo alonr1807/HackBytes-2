1510ff3e3be0fc0b6528c82d92f0afebbb647532
Shreepaad <shreepaad.earanti@gmail.com>
2023-06-30 16:23:40 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+      <img src={logo} className="logo"/>
+        <h1>Ecopact</h1>
       </header>
     </div>
   );
